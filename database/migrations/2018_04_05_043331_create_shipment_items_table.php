43eac3854fef432ca80eed159aee42e2bbf18120
Saurav Bhandari <saurav.bhandari@deerwalk.edu.np>
2018-04-07 08:32:05 +0545

    Added Migration Files

--- database/migrations/2018_04_05_043331_create_shipment_items_table.php
+++ database/migrations/2018_04_05_043331_create_shipment_items_table.php
@@ -18,6 +18,7 @@ class CreateShipmentItemsTable extends Migration
             $table->integer('order_item_id')->unsigned();
             $table->foreign('shipment_id')->references('shipment_id')->on('shipments');
             $table->foreign('order_item_id')->references('order_item_id')->on('order_items');
+            $table->primary(array('shipment_id', 'order_item_id'));
             $table->timestamps();
         });
     }
